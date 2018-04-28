84fef0eff3230a894961c9b4c459d78ae4153727
Jemesson <jemessonlima@gmail.com>
2018-04-28 08:50:58 -0300

    add nickname to chat message

--- chat/app.js
+++ chat/app.js
@@ -1,15 +1,24 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var port = 3000;
 
 app.get('/', (req, res) => {
    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', client => {
-    console.log('You are connected');
+    client.on("join", name => {
+        client.nickname = name;
+    });
+    client.on("messages", msg => {
+        var nickname = client.nickname;
+        var message = nickname + ":" + msg;
+        client.emit('message-name', message);
+        client.broadcast.emit("messages", message);
+    })
 });
 
-http.listen(3000, () => {
-    console.log('I am alive on port 3000');
+http.listen(port, () => {
+    console.log("I am alive on port 3000");
 });
