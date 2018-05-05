10f2b92ed6e1c9cc3019600f7e2dce278c0afe24
Jemesson <jemesson_lima@apple.com>
2018-05-05 08:15:57 -0300

    move views folder to project root

--- src/twitter-api/twitter.js
+++ src/twitter-api/twitter.js
@@ -9,40 +9,37 @@ var app = express();
 const host = '127.0.0.1';
 const port = 3000;
 
-app.get('/tweets/topic/:name', (req, response) => {
-    var name = req.params.name;
-    var options = {
+showTweets = (name, pathUrl, query, response) => {
+    const options = {
         protocol: 'https',
         host: 'api.twitter.com',
-        pathname: '/1.1/search/tweets.json',
-        query: {q: name, result_type: 'popular'}    
+        pathname: pathUrl,
+        query: query    
     };
-    var urlOptions = url.format(options);
+    const urlOptions = url.format(options);
+    
     request(urlOptions, {oauth: config, json: true}, (err, res, data) => {
-        var tweets = data.statuses.map(item => {
-            return item.text;     
-        });
+        var tweets = [];
+
+        if (pathUrl.includes("search")) {
+            tweets = data.statuses.map(item => item.text)        
+        } else if (pathUrl.includes("statuses")) {
+            tweets = data.map(item => item.text);
+        }
+
         response.locals = {tweets: tweets, name: name};
         response.render('tweets.ejs');
     });
+}
+
+app.get('/tweets/topic/:name', (req, response) => {
+    const name = req.params.name;
+    showTweets(name, "/1.1/search/tweets.json", {q: name, result_type: "popular"}, response);
 });
 
 app.get('/tweets/:name', (req, response) => {
-    var name = req.params.name;
-    var options = {
-        protocol: 'https',
-        host: 'api.twitter.com',
-        pathname: '/1.1/statuses/user_timeline.json',
-        query: {screen_name: name, count: 2}    
-    };
-    var urlOptions = url.format(options);
-    request(urlOptions, {oauth: config, json: true}, (err, res, data) => {        
-        const tweets = data.map(item => {
-            return item.text;
-        });
-        response.locals = {tweets: tweets, name: name};
-        response.render("tweets.ejs");
-    });
+    const name = req.params.name;
+    showTweets(name, "/1.1/statuses/user_timeline.json", {screen_name: name, count: 2}, response);
 });
 
 app.listen(port, host, () => {
